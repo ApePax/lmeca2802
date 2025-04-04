49bf7c980cdc116a4fd56a5b9f7becd5b3df1fee
GaXirs <151014420+GaXirs@users.noreply.github.com>
2025-04-04 13:02:31 +0200

    C files generation OK

---
+++ philippides_c/userfctR/user_DrivenJoints.c
@@ -0,0 +1,20 @@
+/* ---------------------------
+ * Robotran - MBsysC
+ * 
+ * Template file for the driven joint function
+ * 
+ * (c) Universite catholique de Louvain
+ *     
+ */
+
+#include "math.h"
+
+#include "mbs_data.h"
+
+
+void user_DrivenJoints(MbsData *mbs_data,double tsim)
+{
+
+}
+
+ 
